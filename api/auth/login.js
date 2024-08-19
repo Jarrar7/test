2fc420a744034882b06b3ea56af52d6f739b2b66
Jarrar7 <100485131+Jarrar7@users.noreply.github.com>
2024-08-19 14:06:08 +0300

    .

--- api/auth/login.js
+++ api/auth/login.js
@@ -2,8 +2,7 @@ import { login } from '../../backend/src/controllers/authController';
 
 export default function handler(req, res) {
 
-    res.setHeader('Access-Control-Allow-Origin', 'https://test-flame-sigma.vercel.app/');
-
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Adjust this as necessary
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
