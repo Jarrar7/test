954d0c9abd94e99a1e79ae2c33d7b51abbfc2bfb
Jarrar7 <100485131+Jarrar7@users.noreply.github.com>
2024-08-21 13:49:56 +0300

    123

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,8 @@
 export default function handler(req, res) {
-    res.status(200).json({ message: "Hello World" });
+    console.log('API handler called');
+    if (req.method === 'GET') {
+        res.status(200).json({ message: 'Hello from API!' });
+    } else {
+        res.status(405).json({ message: 'Method Not Allowed' });
+    }
 }
